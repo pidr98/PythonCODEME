6f68d54add22102839a2ef0ee0c701d04de22c0e
PiotrK <61495748+pidr98@users.noreply.github.com>
2022-05-28 09:58:52 +0200

    0

--- 06_funkcje_cd/kamien_papier_nozyce.py
+++ 06_funkcje_cd/kamien_papier_nozyce.py
@@ -1,12 +1,16 @@
 import random
 
+# wygrany: (przegrany)
 WINNERS = {
-    'k': 'n',
-    'n': 'p',
-    'p': 'k'
+    'k': ('n', 'j'),
+    'n': ('p', 'j'),
+    'p': ('k', 's'),
+    'j': ('p', 's'),
+    's': ('n', 'k')
 }
 
-CORRECT_VALUES = ('k', 'p', 'n')
+CORRECT_VALUES = ('k', 'p', 'n', 'j', 's')
+
 
 def get_comp_choice():
     return random.choice(CORRECT_VALUES)
@@ -14,7 +18,7 @@ def get_comp_choice():
 
 def get_user_choice():
     while True:
-        user_choice = input('Podaj wartość k - kamień, p - papier , n - nożyce: ')
+        user_choice = input('Podaj wartość k - kamień, p - papier , n - nożyce, s - Spock, j - jaszczurka: ')
         if user_choice in CORRECT_VALUES:
             break
 
@@ -24,7 +28,7 @@ def get_user_choice():
 def show_result(comp, user):
     if user == comp:
         print('Remis')
-    elif WINNERS[user] == comp:
+    elif comp in WINNERS[user]:
         print('Wygrywa użytkownika')
     else:
         print('Wygrywa komputer')
