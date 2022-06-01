b0b6142f6087dfea4c27b0ed4e0911015a1a06d6
PiotrK <61495748+pidr98@users.noreply.github.com>
2022-06-01 14:52:18 +0200

    04

--- homework/04/end_09.py
+++ homework/04/end_09.py
@@ -2,8 +2,36 @@
 # Wyświetl najpopularniejszy przedmiot.
 # (Uwzględnij fakt, że użytkownicy mogą zapisać przedmioty małymi, drukowanymi lub zaczynając od dużej litery)
 
-user1 = input('1. Podaj 4 przedmioty szkolne: ')
-user2 = input('2. Podaj 4 przedmioty szkolne: ')
-user3 = input('3. Podaj 4 przedmioty szkolne: ')
-user4 = input('4. Podaj 4 przedmioty szkolne: ')
-user5 = input('5. Podaj 4 przedmioty szkolne: ')
+user1 = input('1. Podaj 4 przedmioty szkolne przedzielone spacją: ')
+user2 = input('2. Podaj 4 przedmioty szkolne przedzielone spacją: ')
+user3 = input('3. Podaj 4 przedmioty szkolne przedzielone spacją: ')
+user4 = input('4. Podaj 4 przedmioty szkolne przedzielone spacją: ')
+user5 = input('5. Podaj 4 przedmioty szkolne przedzielone spacją: ')
+user1 = user1.split()
+user2 = user2.split()
+user3 = user3.split()
+user4 = user4.split()
+user5 = user5.split()
+
+
+all_subjects = user1 + user2 + user3 + user4 + user5
+for i in range(len(all_subjects)):
+    all_subjects[i] = all_subjects[i].lower()
+
+subject_counter = {}
+
+print(all_subjects)
+for subject in all_subjects:
+    if subject in subject_counter:
+        subject_counter[subject] += 1
+    else:
+        subject_counter[subject] = 1
+
+max_value = None
+for k, v in subject_counter.items():
+    if max_value is None or v > max_value:
+        sub = k
+        max_value = v
+
+print(sub, max_value)
+
