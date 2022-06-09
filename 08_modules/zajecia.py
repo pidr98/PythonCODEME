3433b5add86f2d862ab38b3ce8339d65c9e6d688
PiotrK <61495748+pidr98@users.noreply.github.com>
2022-06-09 21:30:32 +0200

    08

---
+++ 08_modules/zajecia.py
@@ -0,0 +1,15 @@
+# script.py
+import maxofthree as m3
+
+def get_three_values():
+  v1 = input('Podaj 1 liczbę całkowitą: ')
+  v2 = input('Podaj 2 liczbę całkowitą: ')
+  v3 = input('Podaj 3 liczbę całkowitą: ')
+  return v1, v2, v3
+
+def main():
+  a, b, c = get_three_values()
+  print(m3.maximum_of(a, b, c))
+
+if __name__ == "__main__":
+  main()
